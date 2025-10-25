10747c18dd6e0520e837945a8f171875845c12e0
Artog Pantheon <artog10@pantheon.io>
2025-10-25 13:00:57 -0400

    added the service to the test class to match the class

--- src/test/java/com/example/maildbshell/ShellControllerTest.java
+++ src/test/java/com/example/maildbshell/ShellControllerTest.java
@@ -3,7 +3,10 @@ package com.example.maildbshell;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.maildbshell.service.EmailService;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -13,6 +16,9 @@ public class ShellControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    
+    @MockBean
+    private EmailService emailService;
 
     @Test
     public void testHelloEndpoint() throws Exception {
